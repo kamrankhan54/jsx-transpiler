2d47e107d0243e91ed5278f2a39f2357535463c1
Kamran Khan <Kam@kamrankhan.local>
2016-04-24 19:59:54 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '/* add jsx here */',
+      output: '',
+      err: ''
+    }
+    this.update = this.update.bind(this)
+  }
+  update(e){
+    let code = e.target.value
+    try {
+      this.setState({
+        output: babel.transform(code, {
+                    stage: 0,
+                    loose: "all"
+                }).code,
+        err: ''
+      })
+    }
+    catch(err){
+      this.setState({
+        err: err.message
+      })
+    }
+  }
+  render(){
+    return (
+      <div>
+        <header>&nbsp;{this.state.err}</header>
+        <div className="container">
+          <textarea onChange={this.update} defaultValue={this.state.input}>
+          </textarea>
+          <pre>
+            {this.state.output}
+          </pre>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+
+### Compile and view JSX code in its true React nature.
+
+```
+npm install
+```
+
+```
+npm start
+```
+
+App runs on port 3333 
